7b0b237c8d38ce54972326efeb6f26f0301f54cc
Hau <phantrunghau1904@gmail.com>
2022-06-30 12:09:35 +0700

    them boi HauHau

---
+++ dangnhap/dangnhap.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Đăng Nhập</title>
+    <link rel="stylesheet" href="dangnhap.css">
+</head>
+
+<body>
+<div class="frame1">
+    <div class="left">
+        <div >
+            <img class="hinhdn" src="hinhaccount.png"/> 
+        </div>
+    </div>
+
+        <div class="right">
+            <div class="a1">
+                <p>QUẢN LÝ NHÂN SỰ</p>
+            </div>
+            <div class="a2">
+                <p>Chào mừng ban đã trở lại!</p>
+            </div>
+            <div>
+                <h2 class="a3">ĐĂNG NHẬP</h2>
+            </div>
+            <form action="" method="post">
+                <div>
+                    <label id="email1" for="email">Email</label>
+                    <input id="email2" name="admin_email" type="text">
+                </div>
+                
+                <div>
+                    <label id="password1" for="password">Mật khẩu</label>
+                    <input id="password2" name="admin_pass" type="password">
+                </div>
+
+                <button class="btn" name="admin_login">Đăng Nhập</button>
+            </form>
+        </div>
+
+    </div>
+
+</body>
+</html>
---
+++ thongtincanhan/thongtin.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Đăng Nhập</title>
+    <link rel="stylesheet" href="thongti.css">
+</head>
+
+<body>
+<div class="frame1">
+    <div class="company">
+        <div  class="a1">COMPANY</div>
+        <P class="a2">Nguyen van a</P>
+    </div>
+    <div class="danhmuc">
+        <img class="menu" src="Menu.png"/>
+        <p class="b1">Danh mục   >   Thông tin cá nhân </p>
+    </div>
+
+    <div class="thongtin">
+        <p class="chucvu">Chổ này là chức vụ</p>
+        <img class="avt" src="avt.png"/>
+        <br>
+        <br>
+        <table>
+            <tr >
+                <td class="c1"> Mã nhân viên: </td>
+                <td class="c2">chổ này là mã nhân viên</td>
+            </tr>
+            <tr >
+                <td class="c1"> Họ và tên:</td>
+                <td class="c2">chổ này là họ tên</td>
+            </tr>
+            <tr >
+                <td class="c1"> Số CCCD/CMT:</td>
+                <td class="c2">chổ này là số cmmd</td>
+            </tr>
+            <tr >
+                <td class="c1"> Giới tính: </td>
+                <td class="c2">chổ này là giới tính</td>
+            </tr>
+            <tr >
+                <td class="c1"> Ngày sinh:</td>
+                <td class="c2">chổ này là ngày sinh</td>
+            </tr>
+            <tr >
+                <td class="c1"> Địa chỉ:</td>
+                <td class="c2">chổ này là địa chỉ chỉ chỉ chỉ chỉ chỉ chỉ chỉ chỉ chỉ chỉ </td>
+            </tr>
+            <tr >
+                <td class="c1"> Số điện thoại: </td>
+                <td class="c2">chổ này là số điện thoai</td>
+            </tr>
+            <tr >
+                <td class="c1"> Email:</td>
+                <td class="c2">chổ này là emailaa</td>
+            </tr>
+           
+        </table>
+      
+                      
+    </div>
+
+    <div class="ttt">
+        <p class="cn">Cập nhật thông tin</p>
+        <table class="table">
+        <form action="" method="post">
+        <tr >
+                <td class="d1"> <img class="av" src="avatar.png"/> </td>
+                <td  >
+                   <div  class="e1"><img class="av1" src="Add Image.png"> <p class="d3">Thêm ảnh</p></div>
+                </td>
+            </tr>
+        </table>
+        <table class="table">
+            <tr >
+                <td class="d1"> Số CCCD/CMT:</td>
+                <td class="d2">chổ này là số cmmd</td>
+            </tr>
+
+            <tr >
+                <td class="d1"> Địa chỉ:</td>
+                <td class="d2">chổ này là địa chỉ chỉ chỉ chỉ chỉ chỉ chỉ chỉ chỉ chỉ chỉ </td>
+            </tr>
+            <tr >
+                <td class="d1"> Số điện thoại: </td>
+                <td class="d2">chổ này là số điện thoai</td>
+            </tr>
+            <tr >
+                <td class="d1"> Email:</td>
+                <td class="d2">chổ này là emailaa</td>
+            </tr>
+           
+        </table>
+        <button class="btnn" name="capnhatthongtin">CẬP NHẬT THÔNG TIN</button>
+        </form>
+    </div>
+        
+    
+
+
+</div>
+</body>
+</html>
