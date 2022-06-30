ec50431242dde4b33d7338cf53df7595387ec73e
Hau <phantrunghau1904@gmail.com>
2022-06-30 14:59:17 +0700

    them boi Hau

---
+++ ql-chucvu/ql-chucvu.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Đăng Nhập</title>
+    <link rel="stylesheet" href="ql-chucvu.css">
+</head>
+
+<body>
+<div class="frame1">
+    <div class="company">
+        <div  class="a1">COMPANY</div>
+        <P class="a2">Nguyen van a</P>
+    </div>
+    <div class="danhmuc">
+        <img class="menu" src="Menu.png"/>
+        <p class="b1">Danh mục   >   Thông tin cá nhân </p>
+    </div>
+
+    <div class="danhsach">
+       <div> <p class="ds">Danh sách chức vụ</p> 
+       <form  action="">
+        <input class="av1" type="text" value="Tìm kiếm">
+        <img class="av2" src="Slider.png" type="button">
+       </form>
+       
+        <img class="av2" src="Search.png">   </div>
+
+        <table class="table" border="1">
+            <tr class="tr1">
+                <td>STT</td>
+                <td>Mã phòng ban</td>
+                <td>Mã chức vụ</td>
+                <td>Tên chức vụ</td>
+                <td>Hệ số lương</td>
+                <td>Tùy chọn</td>
+            </tr>
+        </table>
+    </div>
+    <div class="them">
+        <p class="b2">Thêm trình độ mới</p>
+        <form class="form" >
+            <p class="f1">Tên trình độ</p>
+            <input class="f2" type="text">
+            <p class="f1">Tên trình độ</p>
+            <input class="f2" type="text">
+            <button class="btnn" >Cập nhật thông tin</button>
+
+        </form>
+    </div>
+
+
+   
+        
+    
+
+
+</div>
+</body>
+</html>
